85457669ab4864273aacddd1bed32abc5f58676e
MALLAVALLI APPARAQ <54364675+14761A04E9@users.noreply.github.com>
2020-03-16 13:30:28 +0530

    Completed

--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,9 +1,31 @@
 package utility;
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
+import org.omg.CORBA.portable.InputStream;
 
-
-
-public class ConnectionManager {
+public class ConnectionManager
+{
 	
+	public  static Connection getConnection() throws SQLException, ClassNotFoundException 
+	 {
+		 Class.forName("oracle.jdbc.OracleDriver");
+		 Connection con;
+		 con=DriverManager.getConnection("jdbc:oracle:thin:SYSTEM/root@localhost:1521:xe","system","apparao@E9");
+		 
+		 return con;
+	 }
+	 public   Properties  loadPropertiesFile() throws IOException 
+	 {
+		 Properties prop = new Properties();
+		 InputStream in = (InputStream) ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		 prop.load(in);
+		 in.close();
+		 return prop;
+		 
+	 }
 	
 }
